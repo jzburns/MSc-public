ee43657ed65297dcff05e0aa706071d79827c0cd
John Burns <jzburns@gmail.com>
2022-03-06 20:33:47 +0000

    added some TODOs

---
+++ serverless-intro/main.tf
@@ -0,0 +1,39 @@
+provider "google" {
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = format("%s-%s", var.project_id, "cloud-functions-bucket")
+  location = "eu"
+}
+
+resource "google_storage_bucket_object" "archive" {
+  name   = "index.zip"
+  bucket = google_storage_bucket.bucket.name
+  #############################################################
+  ## TODO: change 'YOUR-HOME-DIR' to your home directory
+  #############################################################
+  source = "/home/YOUR-HOME-DIR/MSc-public/serverless-intro/gcf/index.zip"
+}
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "hello-world"
+  description = "Hello World Function"
+  runtime     = "nodejs14"
+
+  available_memory_mb   = 128
+  source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_object = google_storage_bucket_object.archive.name
+  trigger_http          = true
+  entry_point           = "helloGET"
+  region                = var.region
+}
+
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = var.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
---
+++ serverless-intro/variables.tf
@@ -0,0 +1,35 @@
+#######################################################################
+## TODO: change 'YOUR-PROJ-ID' to your own project ID
+#######################################################################
+variable "project_id" {
+  description = "The project ID to deploy resources into"
+  default   = "YOUR-PROJ-ID"
+}
+
+variable "subnetwork" {
+  description = "The name of the subnetwork to deploy instances into"
+  default = "default"
+}
+
+variable "instance_name" {
+  description = "The desired name to assign to the deployed instance"
+  default     = "container-vm-terraform"
+}
+
+variable "zone" {
+  description = "The GCP zone to deploy instances into"
+  type        = string
+  default     = "europe-west1-c"
+}
+
+variable "region" {
+  description = "The GCP region for the functions"
+  type        = string
+  default     = "europe-west1"
+}
+
+variable "client_email" {
+  description = "Service account email address"
+  type        = string
+  default     = ""
+}
