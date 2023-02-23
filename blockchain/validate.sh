08f9b090411d23a07c1a3752c908746d77b1d5fe
John Burns <jzburns@gmail.com>
2023-02-23 10:55:35 +0000

    updates

--- blockchain/validate.sh
+++ blockchain/validate.sh
@@ -13,7 +13,7 @@ echo $1 > $nonce_file
 attempt=$(cat data.txt $nonce_file| exec sha256sum)
 echo "Checking $1 $attempt"
 if [ ${attempt:0:$num_zeros} = $difficulty ]; then
-	echo "Correct! I accept your $nonce"
+	echo "Correct! I accept your nonce $1"
 	exit 0;
 else
 	echo "Wrong! try again"
