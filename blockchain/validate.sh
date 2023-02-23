ff4f7cbe3ab800bf6917b43729596f5235c34ad4
John Burns <jzburns@gmail.com>
2023-02-23 10:02:29 +0000

    added the block validator

---
+++ blockchain/validate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+i=1
+
+## write the nonce here
+nonce_file=proposed-nonce.txt
+
+## change these two parameters together
+difficulty=0
+num_zeros=1
+ 
+echo $1 > $nonce_file
+attempt=$(cat data.txt $nonce_file| exec sha256sum)
+echo "Checking $1 $attempt"
+if [ ${attempt:0:$num_zeros} = $difficulty ]; then
+	echo "Correct! I accept your $nonce"
+	exit 0;
+else
+	echo "Wrong! try again"
+fi
