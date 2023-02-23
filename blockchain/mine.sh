8a707f932aee1764415679d1a7db25e114d026bf
John Burns <jzburns@gmail.com>
2023-02-23 09:33:08 +0000

    simple blockchain mining demo

---
+++ blockchain/mine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+i=1
+
+## write the nonce here
+nonce_file=nonce.txt
+
+## change these two parameters together
+difficulty=0
+num_zeros=1
+ 
+while true
+do
+		nonce=$(echo $RANDOM | md5sum)
+		echo $nonce > $nonce_file
+		attempt=$(cat data.txt $nonce_file| exec sha256sum)
+		echo "$nonce $attempt"
+		if [ ${attempt:0:$num_zeros} = $difficulty ]; then
+			echo "Mined a block after $i attempts! using nonce $nonce"
+			exit 0;
+		fi
+		sleep 1
+   let i++
+done
