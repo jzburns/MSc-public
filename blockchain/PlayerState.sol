70092b1bea5cedd243072c936476f078933a81a1
Dr. John Burns <jzburns@users.noreply.github.com>
2024-02-04 10:14:10 +0000

    Update PlayerState.sol

--- blockchain/PlayerState.sol
+++ blockchain/PlayerState.sol
@@ -8,6 +8,7 @@ contract PlayerStateManagement {
 
    // A uint is an unsigned integer in the range 0 to 2^64 -1
    uint public numberOfCalls = 0;
+   uint public donatatedBalance = 0;
 
    // A mapping is a K:V pair dictionart (similar to python dict)
    mapping(address => uint) public playerGameSate;
@@ -22,14 +23,21 @@ contract PlayerStateManagement {
   function showCallerAddress () external view returns (address) {
     return msg.sender;
   }
+  
+  receive() external payable {
+    // This function is executed when a contract receives plain Ether (without data)
+  }
+
+  function donate() external payable {
+    // This function is executed when a contract receives plain Ether (without data)
+    donatatedBalance += msg.value;
+  }
 
   // Payable functions in Solidity are functions that let a smart contract accept Ether
-  function updatePlayerState (uint newState) external payable {
-
-    // f we had a smart contract where we needed to keep track of who deposited which ether, we might keep track of that in storage:
+  function updatePlayerState (uint newState) external  {
+    //if we had a smart contract where we needed to keep track of who deposited which ether, we might keep track of that in storage:
     playerGameSate[msg.sender] += newState;
     numberOfCalls++;
-  
   }
 
 }
